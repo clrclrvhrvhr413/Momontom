ebcf99c9fd7df29718878a2c3fb1c3447a1133c0
clrclrvhrvhr413 <clrclrvhrvhr413@gmail.com>
2021-04-29 21:28:12 +0900

    add logout btn in welcome func

--- src/welcome.js
+++ src/welcome.js
@@ -1,19 +1,33 @@
 const div = document.querySelector(".welcomeContainer"),
     askForm = div.querySelector(".js-userNameForm"),
-    userName = askForm.querySelector("#userName")
-    welcome = div.querySelector(".welcome");
+    userName = askForm.querySelector("#userName");
+const div2 = div.querySelector(".logContainer"),
+    welcome = div2.querySelector(".welcome"),
+    btn = div2.querySelector("#logout");
 
 const USER = "userName",
     SHOW_CLS = "showing";
 
+function logoutUser(){
+    localStorage.removeItem(USER);
+    div2.classList.remove(SHOW_CLS);
+    askForm.classList.add(SHOW_CLS);
+    userName.value="";
+}
+function handleLogout(event){
+    event.preventDefault();
+    logoutUser();
+}
 function saveUser(name){
     localStorage.setItem(USER,name);
 }
 function welcomeUser(name){
     askForm.classList.remove(SHOW_CLS);
-    welcome.classList.add(SHOW_CLS);
+    div2.classList.add(SHOW_CLS);
     welcome.innerHTML = `WELCOME ${name}`;
     saveUser(name);
+
+    btn.addEventListener("click",handleLogout);
 }
 function handleSubmit(event){
     event.preventDefault();
